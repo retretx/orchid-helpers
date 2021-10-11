ec90a6096da6172303936b5f93ea1ad951cfd4b6
rrmode <retretx@gmail.com>
2021-10-11 19:03:30 +0500

    Initial (requires macro for Action)

---
+++ src/HelpersProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Easy\OrchidHelpers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+use Orchid\Screen\Action;
+
+class HelpersProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->requiresMacro();
+    }
+
+    public function requiresMacro(): void
+    {
+        Action::macro(name: 'requires', macro: function (string $permission) {
+            $this->canSee(value: Auth::user()?->hasAccess(permit: $permission) ?? false);
+        });
+    }
+
+}
